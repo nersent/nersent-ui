a303b03d6f3606025e5b471921d6c88684732bb6
xNerhu22 <xnerhu22@onet.pl>
2017-02-16 11:45:27 +0100

    Added more options to text field

--- components/textfield.js
+++ components/textfield.js
@@ -24,7 +24,7 @@ export default class TextField extends React.Component {
     onFocus(e) {
         var t = this;
         if (this.refs.input.value.length < 1) {
-            TweenMax.to(this.refs.hint, 0.15, {
+            TweenMax.to(this.refs.hint, this.props.focusedHintAnimationTime, {
                 css: {
                     fontSize: this.props.focusedHintFontSize,
                     top: this.props.focusedHintMarginTop,
@@ -32,7 +32,7 @@ export default class TextField extends React.Component {
                     opacity: this.props.focusedHintOpacity
                 }
             });
-            TweenMax.to(this.refs.focus_divider, 0.15, {
+            TweenMax.to(this.refs.focus_divider, this.props.focusedDividerAnimationTime, {
                 css: {
                     width: '100%'
                 }
@@ -47,7 +47,7 @@ export default class TextField extends React.Component {
     onFocusOut(e) {
         var t = this;
         if (this.refs.input.value.length < 1) {
-            TweenMax.to(this.refs.hint, 0.2, {
+            TweenMax.to(this.refs.hint, this.props.hintAnimationTime, {
                 css: {
                     fontSize: this.props.hintFontSize,
                     top: this.props.hintMarginTop,
@@ -55,7 +55,7 @@ export default class TextField extends React.Component {
                     opacity: this.props.hintOpacity
                 }
             });
-            TweenMax.to(this.refs.focus_divider, 0.15, {
+            TweenMax.to(this.refs.focus_divider, this.props.dividerAnimationTime, {
                 css: {
                     width: '0%'
                 }
@@ -78,9 +78,9 @@ export default class TextField extends React.Component {
         return (
             <div className="material-textfield" style={this.props.style}>
                 <input ref="input" onClick={this.onFocus} onBlur={this.onFocusOut} style={{fontSize: this.props.fontSize, color: this.props.color, paddingBottom: this.props.inputPaddingBottom}}></input>
-                <div className="hint" ref="hint" style={{fontSize: this.props.hintFontSize, color: this.props.hintColor, opacity: this.props.hintOpacity, top: this.props.hintMarginTop}} onClick={this.onHintClick}>Hello world!</div>
-                <div className="divider" ref="divider"></div>
-                <div className="focus_divider" ref="focus_divider"></div>
+                <div className="hint" ref="hint" style={{fontSize: this.props.hintFontSize, color: this.props.hintColor, opacity: this.props.hintOpacity, top: this.props.hintMarginTop}} onClick={this.onHintClick}>{this.props.hintText}</div>
+                <div className="divider" ref="divider" style={{backgroundColor: this.props.dividerColor, opacity: this.props.dividerOpacity}}></div>
+                <div className="focus_divider" ref="focus_divider" style={{backgroundColor: this.props.focusedDividerColor}}></div>
             </div>
         );
     }
@@ -89,15 +89,23 @@ export default class TextField extends React.Component {
 TextField.defaultProps = {
     fontSize: 18,
     color: "#444",
+    hintText: "",
     inputPaddingBottom: 6,
     hintFontSize: 18,
     hintOpacity: 0.4,
     hintColor: "#444",
     hintMarginTop: 0,
+    hintAnimationTime: 0.2,
+    dividerAnimationTime: 0.15,
+    dividerOpacity: 0.2,
+    dividerColor: "#000",
     focusedHintFontSize: 14,
     focusedHintOpacity: 1,
     focusedHintColor: "#03a9f4",
-    focusedHintMarginTop: -18
+    focusedHintMarginTop: -18,
+    focusedHintAnimationTime: 0.15,
+    focusedDividerAnimationTime: 0.15,
+    focusedDividerColor: "#03a9f4"
 };
 /*
     this.refs.textarea.style.height = "auto";
