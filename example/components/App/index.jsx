6be3a9602f1e42e016c93439c5ba2f915ac8af49
Mikołaj Palkiewicz <xnerhu22@onet.pl>
2018-02-10 21:48:40 +0100

    Add flat button

--- example/components/App/index.jsx
+++ example/components/App/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 import RaisedButton from '../../../material-react/components/RaisedButton'
+import FlatButton from '../../../material-react/components/FlatButton'
 
 export default class App extends React.Component {
   render () {
-    const onClick = () => {
-      console.log('Click!')      
-    }
-
     return (
       <div className='app-container'>
-        <RaisedButton background='#2196F3' foreground={true} darkTheme={true} onClick={onClick}>
-          button
+        <RaisedButton>
+          raised button
         </RaisedButton>
+        <FlatButton>
+          flat button
+        </FlatButton>
       </div>
     )
   }
---
+++ material-react/components/FlatButton/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import Ripple from '../Ripple'
+
+import ClassManager from '../../utils/class'
+
+export default class FlatButton extends React.Component {
+  render () {
+    const {
+      color,
+      ripple,
+      className,
+      children,
+      disabled,
+      darkTheme,
+      onClick,
+      onMouseEnter,
+      onMouseLeave
+    } = this.props
+
+    const style = {
+      color: color
+    }
+
+    const events = {
+      onClick: onClick,
+      onMouseEnter: onMouseEnter,
+      onMouseLeave: onMouseLeave
+    }
+
+    const rootClass = ClassManager.get('material-button flat', [
+      className,
+      disabled ? 'disabled' : '',
+      darkTheme ? 'dark-theme' : ''
+    ])
+
+    return (
+      <div className={rootClass} style={style} {...events}>
+        {children}
+        <div className='over-shade' />
+        <Ripple autoClass={false} autoRipple={!disabled} color={color} options={ripple} />
+      </div>
+    )
+  }
+}
+
+FlatButton.defaultProps = {
+  color: '#3F51B5',
+  disabled: false,
+  darkTheme: false
+}
