b518b4ce477a2aae1b7e53a9cbe1111bfcf61728
Sential <sentialx@gmail.com>
2018-03-16 23:17:23 +0100

    :art: Fix fonts

--- src/mixins/typography.ts
+++ src/mixins/typography.ts
@@ -1,28 +1,28 @@
 const body1 = () => `
   font-size: 13px;
-  font-family: Roboto, sans-serif;
+  font-family: Roboto;
   font-weight: 400;
 `;
 
 const body2 = () => `
   font-size: 14px;
-  font-family: Roboto, sans-serif;
+  font-family: Roboto;
   font-weight: 500;
 `;
 
 const button = () => `
   font-size: 14px;
-  font-family: Roboto, sans-serif;
+  font-family: Roboto;
   font-weight: 500;
 `;
 
 const robotoRegular = () => `
-  font-family: Roboto, sans-serif;
+  font-family: Roboto;
   font-weight: 400;
 `;
 
 const robotoMedium = () => `
-  font-family: Roboto, sans-serif;
+  font-family: Roboto;
   font-weight: 500;
 `;
 
